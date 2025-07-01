ea89682da57a6ba8d2e96aba9d49ab103420c40c
Vidisha Srivastava <vidishasrivastava2001@gmail.com>
2025-07-01 18:04:02 +0530

    creating routes

---
+++ reactRouter/src/App.jsx
@@ -0,0 +1,30 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import './App.css'
+import { Home } from './pages/Home';
+import { About } from './pages/About';
+import { Movies } from './pages/Movies';
+import { Contact } from './pages/Contact';
+
+const App = () => {
+  const router = createBrowserRouter([
+    {
+      path:"/",
+      element:<Home />,
+    },
+     {
+      path:"/about",
+      element:<About/>,
+    },
+     {
+      path:"/movies",
+      element:<Movies />,
+    },
+     {
+      path:"/contact",
+      element:<Contact />,
+    },
+  ]);
+  return <RouterProvider router={router} />;
+};
+
+export default App
---
+++ reactRouter/src/pages/Movies.jsx
@@ -0,0 +1,3 @@
+export const Movies = () => {
+  return <h1>Movie Page</h1>
+};
